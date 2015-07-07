b26b09ea923c908455e47c58d578b6986b2eb98e
Philippe Luchansky <philuchansky@gmail.com>
2015-07-07 00:37:46 -0700

    test block styles for collision remove unused console logs inverse values for checkOutOfBounds() / checkCollision() evaluation basic line / rectangle intersection detection with checkIntersection() animate #testBlock up to make sure collision works with each frame's updated coordinates

--- js/app.js
+++ js/app.js
@@ -9,8 +9,24 @@ $(function(){
     var p2W = $(p2).width();
     var p2H = p2W;
 
+    var testBlock = $('#testBlock');
+    var blockBLX = null;
+    var blockTLY = null;
+    var blockTRX = null;
+    var blockBRY = null;
+
+    //line
     var line = $('#line');
 
+    var lineP1X = null;
+    var lineP1Y = null;
+    var lineP2X = null;
+    var lineP2Y = null;
+
+    var lineP1 = [];
+    var lineP2 = [];
+
+    //global speed
     var speed = 3;
 
     //player move states:
@@ -32,23 +48,22 @@ $(function(){
     $(g1).css({'left': $(p1).position().left, 'top': $(p1).position().top});
     $(g2).css({'left': $(p2).position().left, 'top': $(p2).position().top});
 
-
     //player 1 key listeners
     $(body).keydown(function(p1) {
         // prevent arrow keys from scrolling the page
         //event.preventDefault();
         p1k = p1.keyCode;
-        if (p1k==65) {console.log('left down');p1MoveLeft = true}
-        if (p1k==68) {console.log('right down');p1MoveRight = true}
-        if (p1k==87) {console.log('up down');p1MoveUp = true}
-        if (p1k==83) {console.log('down down');p1MoveDown = true}
+        if (p1k==65) {p1MoveLeft = true}
+        if (p1k==68) {p1MoveRight = true}
+        if (p1k==87) {p1MoveUp = true}
+        if (p1k==83) {p1MoveDown = true}
     });
     $(body).keyup(function(p1) {
         p1k = p1.keyCode;
-        if (p1k==65) {console.log('left up');p1MoveLeft = false}
-        if (p1k==68) {console.log('right up');p1MoveRight = false}
-        if (p1k==87) {console.log('up up');p1MoveUp = false}
-        if (p1k==83) {console.log('down up');p1MoveDown = false}
+        if (p1k==65) {p1MoveLeft = false}
+        if (p1k==68) {p1MoveRight = false}
+        if (p1k==87) {p1MoveUp = false}
+        if (p1k==83) {p1MoveDown = false}
     });
 
     //player 2 key listeners
@@ -56,17 +71,17 @@ $(function(){
         // prevent arrow keys from scrolling the page
         //event.preventDefault();
         p2k = p2.keyCode;
-        if (p2k==37) {console.log('left down');p2MoveLeft = true;}
-        if (p2k==39) {console.log('right down');p2MoveRight = true;}
-        if (p2k==38) {console.log('up down');p2MoveUp = true;}
-        if (p2k==40) {console.log('down down');p2MoveDown = true;}
+        if (p2k==37) {p2MoveLeft = true;}
+        if (p2k==39) {p2MoveRight = true;}
+        if (p2k==38) {p2MoveUp = true;}
+        if (p2k==40) {p2MoveDown = true;}
     });
     $(body).keyup(function(p2) {
         p2k = p2.keyCode;
-        if (p2k==37) {console.log('left up');p2MoveLeft = false;}
-        if (p2k==39) {console.log('right up');p2MoveRight = false;}
-        if (p2k==38) {console.log('up up');p2MoveUp = false;}
-        if (p2k==40) {console.log('down up');p2MoveDown = false;}
+        if (p2k==37) {p2MoveLeft = false;}
+        if (p2k==39) {p2MoveRight = false;}
+        if (p2k==38) {p2MoveUp = false;}
+        if (p2k==40) {p2MoveDown = false;}
     });
 
     function alignLine() {
@@ -79,6 +94,21 @@ $(function(){
         $(line).attr('y1',player1CenterY);
         $(line).attr('x2', player2CenterX);
         $(line).attr('y2',player2CenterY);
+
+        lineP1X = $(line).attr('x1');
+        lineP1Y = $(line).attr('y1');
+        lineP2X = $(line).attr('x2');
+        lineP2Y = $(line).attr('y2');
+
+        lineP1 = [lineP1X,lineP1Y];
+        lineP2 = [lineP2X,lineP2Y];
+
+        //length of line:
+/*
+        var lineW = Math.abs($(line).attr('x1') - $(line).attr('x2'));
+        var lineH = Math.abs($(line).attr('y1') - $(line).attr('y2'));
+        var lineLength = Math.round(Math.sqrt((lineW * lineW) + (lineH * lineH)));
+*/
     }
 
     function checkOutOfBounds(obj,bounds) {
@@ -92,7 +122,7 @@ $(function(){
         var boundsTop = 0;
         var boundsBottom = bounds.height();
 
-        return !(objLeft < boundsLeft || objRight > boundsRight || objTop < boundsTop || objBottom > boundsBottom);
+        return (objLeft < boundsLeft || objRight > boundsRight || objTop < boundsTop || objBottom > boundsBottom);
     }
 
     function checkCollision(obj1,obj2) {
@@ -110,33 +140,80 @@ $(function(){
         var bottom2 = top2 + obj2.height();
 
         return ((right1 > left2 && (bottom1 > top2 && top1 < bottom2)) && (left1 < right2 && (top1 < bottom2 && bottom1 > top2)));
+    }
 
+    /********** Collision detection for line **********/
+    function checkCorners(x,y) {
+        return (((lineP2Y - lineP1Y) * x) + ((lineP1X - lineP2X) * y)) + ((lineP2X * lineP1Y) - (lineP1X * lineP2Y));
+    }
+    function checkIntersection() {
+        var cornerCheckTL = checkCorners(blockBLX,blockBRY);
+        var cornerCheckTR = checkCorners(blockTRX,blockBRY);
+        var cornerCheckBL = checkCorners(blockBLX,blockTLY);
+        var cornerCheckBR = checkCorners(blockTRX,blockTLY);
+        if((cornerCheckBL < 0 && cornerCheckBR < 0 && cornerCheckTL < 0 && cornerCheckTR < 0) || (cornerCheckBL > 0 && cornerCheckBR > 0 && cornerCheckTL > 0 && cornerCheckTR > 0)) {
+            console.log('no intersection :(');
+            return false;
+        } else if((lineP1X > blockTRX && lineP2X > blockTRX) || (lineP1X < blockBLX && lineP2X < blockBLX) || ((lineP1Y > blockBRY && lineP2Y > blockBRY)) || (lineP1Y < blockTLY && lineP2Y < blockTLY)) {
+            console.log('no intersection :(');
+            return false;
+        } else {
+            console.log('WE HAVE INTERSECTION!!!');
+        }
+    }
+    /********** Collision detection for line **********/
 
+    function moveTestBlock() {
+        $(testBlock).css({'top': $(testBlock).position().top -.5});
+        blockBLX = $(testBlock).position().left;
+        blockTLY =  $(testBlock).position().top;
+        blockTRX = $(testBlock).position().left + $(testBlock).width();
+        blockBRY = $(testBlock).position().top + $(testBlock).height();
+
+        checkIntersection();
     }
 
     function tick() {
-        if(p1MoveLeft) {$(p1).css({'left': '-=' + speed});}
-        if(p1MoveRight) {$(p1).css({'left': '+=' + speed});}
-        if(p1MoveUp) {$(p1).css({'top': '-=' + speed});}
-        if(p1MoveDown) {$(p1).css({'top': '+=' + speed});}
-
-        if(p2MoveLeft) {$(p2).css({'left': '-='+speed});}
-        if(p2MoveRight) {$(p2).css({'left': '+=' + speed});}
-        if(p2MoveUp) {$(p2).css({'top': '-=' + speed});}
-        if(p2MoveDown) {$(p2).css({'top': '+=' + speed});}
-
-        alignLine();
-        console.log();
-        if(checkCollision($(p1),$('#testBlock'))) {
-            console.log('COLLISION DETECTED!');
+        var p1Moved = 0;
+        if(p1MoveLeft) {$(g1).css({'left': '-=' + speed}); p1Moved = 1}
+        if(p1MoveRight) {$(g1).css({'left': '+=' + speed}); p1Moved = 1}
+        if(p1MoveUp) {$(g1).css({'top': '-=' + speed}); p1Moved = 1}
+        if(p1MoveDown) {$(g1).css({'top': '+=' + speed}); p1Moved = 1}
+        if(p1Moved) {
+            if(!checkOutOfBounds($(g1), $(diploid))) {
+                $(p1).css({'top': $(g1).position().top, 'left': $(g1).position().left});
+            } else {
+                $(g1).css({'top': $(p1).position().top, 'left': $(p1).position().left});
+            }
         }
+
+        var p2Moved = 0;
+        if(p2MoveLeft) {$(g2).css({'left': '-='+speed}); p2Moved = 1}
+        if(p2MoveRight) {$(g2).css({'left': '+=' + speed}); p2Moved = 1}
+        if(p2MoveUp) {$(g2).css({'top': '-=' + speed}); p2Moved = 1}
+        if(p2MoveDown) {$(g2).css({'top': '+=' + speed}); p2Moved = 1}
+        if(p2Moved) {
+            if(!checkOutOfBounds($(g2), $(diploid))) {
+                $(p2).css({'top': $(g2).position().top, 'left': $(g2).position().left});
+            } else {
+                $(g2).css({'top': $(p2).position().top, 'left': $(p2).position().left});
+            }
+        }
+
+        if(checkCollision($(p1),$(testBlock))) {
+            console.log('Player 1 collided with a block!');
+        }
+        if(checkCollision($(p2),$(testBlock))) {
+            console.log('Player 2 Collided with a block!');
+        }
+        alignLine();
+        moveTestBlock();
     }
 
     //initialize the game
     function initDiploid() {
         alignLine();
         startGame = setInterval(tick, 10);
-        console.log($(p1));
     }
 
     //stop game with spacebar
